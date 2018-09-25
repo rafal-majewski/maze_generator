520f407a5f19187a99e8bfc246f4e4bda54f8bf1
Rafał Majewski <31292304+RafalMajewskiPL@users.noreply.github.com>
2018-09-25 18:48:39 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,245 +1,96 @@
-/*
-ENG:
-Created by Rafał Majewski (GHPL) in 2017
-
-Condition of use:
-If this code (modified or not) is distributed in uncompiled form then there must be information about original author.
-
-
-PL:
-Stworzone przez Rafała Majewskiego (GHPL) in 2017
-
-Warunek używania:
-Jeżeli ten kod (zmodyfikowany lub nie) jest rozprowadzany ze zmianami lub bez w postaci nieskompilowanej to trzeba podać informację o oryginalnym autorze.
-*/
-
 #include <iostream>
 #include <map>
-#include <stdlib.h>
-#include "gyhLib.h"
-#include <fstream>
-#include <ctime>
-#include <cstdio>
+#include <time.h>
 
+std::map<short, std::map<short, bool>> maze; //true - wall, false - space
+short sizeX=11, sizeY=14; //have to be odd
 
-unsigned int xSize=27;
-unsigned int ySize=25;
-unsigned int seed=2.137;
-int los;
-
-unsigned long long bajtyW=0;
-unsigned long long bajtyZ=0;
-unsigned long long dozapis=(xSize+2)*(ySize+2)+(ySize+2);
-unsigned long long dowygen=(xSize+2)*(ySize+2)+2*((xSize-1)/2)*((ySize-1)/2);
-
-void clear()
+char dx(char direction)
 {
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
+    if (direction == 1) return 1;
+    else if (direction == 3) return -1;
+    else return 0;
 }
 
-int dx(int p)
+char dy(char direction)
 {
-    if (p==0) return 1;
-    if (p==1) return 0;
-    if (p==2) return -1;
-    if (p==3) return 0;
+    if (direction == 0) return -1;
+    else if (direction == 2) return 1;
+    else return 0;
 }
 
-int dy(int p)
+//check if a closed room will be created
+bool check(short x, short y, char direction, short s_x, short s_y)
 {
-    if (p==0) return 0;
-    if (p==1) return 1;
-    if (p==2) return 0;
-    if (p==3) return -1;
-}
-
-int f1(int p)
-{
-    return mod(p+1,4);
-}
-
-int f2(int p)
-{
-    return mod(p-1,4);
-}
-
-std::map<int,std::map<int,int> >mapa;
-//std::map<int,std::map<int,int> >kierunki;
-
-bool flaga=false;
-
-bool check(int cX,int cY,int stX,int stY,int dir,int iter,int a)
-{
-    if (iter==0) flaga=false;
-    bool ret=true;
-    int i=0;
-
-    while (mapa[stX+i*dx(dir)][stY+i*dy(dir)]==-1)
+    if (maze[x][y] == 0 || x == 0 || x == sizeX-1 || y == 0 || y == sizeY-1) return false;
+    if (x == s_x && y == s_y) return true;
+    if (maze[x+dx((direction+3)%4)][y+dy((direction+3)%4)] == 1)
     {
-        if (flaga==true) break;
-        //std::cout<<cX<<"."<<cY<<" "<<a<<" iter"<<iter<<" "<<stX+i*dx(dir)<<"."<<stY+i*dy(dir)<<std::endl;
-        if (iter==3)
-        {
-            if (stX+i*dx(dir)==cX&&stY+i*dy(dir)==cY)
-            {
-                //std::cout<<"falsz"<<std::endl;
-                flaga=true;
-                return false;
-            }
-        }
-        if (iter<3)
-        {
-            if (mapa[stX+i*dx(dir)+dx(mod(dir+a,4))][stY+i*dy(dir)+dy(mod(dir+a,4))]==-1)
-            {
-               ret=check(cX,cY,stX+i*dx(dir)+dx(mod(dir+a,4)),stY+i*dy(dir)+dy(mod(dir+a,4)),mod(dir+a,4),iter+1,a);
-            }
-        }
-        i++;
+        if (check(x+dx((direction+3)%4), y+dy((direction+3)%4), (direction+3)%4, s_x, s_y)) return true;
     }
-    return ret;
+    if (maze[x+dx(direction)][y+dy(direction)] == 1)
+    {
+        if (check(x+dx(direction), y+dy(direction), direction, s_x, s_y)) return true;
+    }
+    if (maze[x+dx((direction+1)%4)][y+dy((direction+1)%4)] == 1)
+    {
+        if (check(x+dx((direction+1)%4), y+dy((direction+1)%4), (direction+1)%4, s_x, s_y)) return true;
+    }
+    return false;
 }
 
-int rind=1;
-
-main()
+int main()
 {
-    srand(seed);
-    std::fstream plik;
-    plik.open("labirynt.txt",std::ios::out);
-
-    for (int iy=0;iy<=ySize+1;iy++)
+    srand(time(NULL));
+    //fill map with zeros
+    for (short y=0; y<sizeY; ++y)
     {
-        mapa[-1][iy-1]=-2;
-        bajtyW++;
-        //std::cout<<bajtyW*100/dowygen<<"% wygenerowano"<<std::endl;
-        //clear();
-        mapa[xSize][iy-1]=-2;
-        bajtyW++;
-        //std::cout<<bajtyW*100/dowygen<<"% wygenerowano"<<std::endl;
-        //clear();
-        //kierunki[-1][iy-1]=-2;
-        //kierunki[xSize][iy-1]=-2;
-
-    }
-    for (int ix=0;ix<=xSize-1;ix++)
-    {
-        mapa[ix][-1]=-2;
-        bajtyW++;
-        //std::cout<<bajtyW*100/dowygen<<"% wygenerowano"<<std::endl;
-        //clear();
-        mapa[ix][ySize]=-2;
-        bajtyW++;
-        //std::cout<<bajtyW*100/dowygen<<"% wygenerowano"<<std::endl;
-        //clear();
-
-        //kierunki[ix][-1]=-2;
-        //kierunki[ix][ySize]=-2;
-    }
-
-
-    for (int iy=0;iy<ySize;iy++)
-    {
-        for (int ix=0;ix<xSize;ix++)
+        for (short x=0; x<sizeX; ++x)
         {
-            mapa[ix][iy]=0;
-            //kierunki[ix][iy]=0;
-            bajtyW++;
-            //std::cout<<bajtyW*100/dowygen<<"% wygenerowano"<<std::endl;
-            //clear();
+            maze[x][y]=0;
         }
     }
-
-    for (int iy=1;iy<ySize;iy+=2)
+    //make walls on edges
+    for (short y=0; y<sizeY; ++y)
     {
-        for (int ix=1;ix<xSize;ix+=2)
+        maze[0][y]=1;
+    }
+    for (short x=1; x<sizeX; ++x)
+    {
+        maze[x][0]=1;
+    }
+    for (short y=1; y<sizeY; ++y)
+    {
+        maze[sizeX-1][y]=1;
+    }
+    for (short x=1; x<sizeX; ++x)
+    {
+        maze[x][sizeY-1]=1;
+    }
+    //generate
+    for (short y=2; y<sizeY-2; y+=2)
+    {
+        for (short x=2; x<sizeX-2; x+=2)
         {
-            mapa[ix][iy]=-1;
-            bajtyW++;
-            //std::cout<<bajtyW*100/dowygen<<"% wygenerowano"<<std::endl;
-            //clear();
-            //kierunki[ix][iy]=-1;
-            los=rand()%4;
-            //los=random(0,3,rind,seed);
-            rind++;
-            for (int i=0;i<4;i++)
+            char direction=rand()%4; //choose a random direction
+            maze[x][y]=1; //make wall on the starting point
+            while (maze[x+dx(direction)][y+dy(direction)] == 1 || check(x+2*dx(direction), y+2*dy(direction), direction, x, y)) //change direction until it will be possible to make a wall
             {
-                //std::cout<<"a dostalem z 1 "<<check(ix,iy,ix+2*dx(los),iy+2*dy(los),los,0,1)<<std::endl;
-                //std::cout<<"a dostalem z -1 "<<check(ix,iy,ix+2*dx(los),iy+2*dy(los),los,0,-1)<<std::endl;
-                if (mapa[ix+dx(los)][iy+dy(los)]==0&&check(ix,iy,ix+2*dx(los),iy+2*dy(los),los,0,1)&&check(ix,iy,ix+2*dx(los),iy+2*dy(los),los,0,-1))
-                {
-                    mapa[ix+dx(los)][iy+dy(los)]=-1;
-                    //kierunki[ix+dx(los)][iy+dy(los)]=los+1;
-                    bajtyW++;
-                    //std::cout<<bajtyW*100/dowygen<<"% wygenerowano"<<std::endl;
-                    //clear();
-                    break;
-                }
-                else
-                {
-                    los=mod(los+1,4);
-                }
+                ++direction;
+                direction=direction%4;
             }
+            maze[x+dx(direction)][y+dy(direction)]=1;
         }
     }
-
-    for (int iy=0;iy<=ySize+1;iy++)
+    //draw
+    for (short y=0; y<sizeY; ++y)
     {
-        bajtyZ++;
-        for (int ix=0;ix<=xSize+1;ix++)
+        for (short x=0; x<sizeX; ++x)
         {
-            if (mapa[ix-1][iy-1]==-1)
-            {
-                //std::cout<<"#";
-                plik<<"#";
-                bajtyZ++;
-            }
-            else if (mapa[ix-1][iy-1]==-2)
-            {
-                //std::cout<<"$";
-                plik<<"$";
-                bajtyZ++;
-            }
-            else
-            {
-                //std::cout<<" ";
-                plik<<" ";
-                bajtyZ++;
-
-            }
-            //std::cout<<bajtyZ*100/dozapis<<"% zapisano "<<bajtyZ/1048576<<" megabajtow"<<std::endl;
-            //clear();
-            //std::cout<<bajtyZ*100/dozapis<<"% zapisano "<<(long double)bajtyZ/1048576<<" megabajtow"<<std::endl;
-            //clear();
-            //std::cout<<bajtyZ*100/dozapis<<"% zapisano "<<(long double)bajtyZ/1024<<" kilobajtow"<<std::endl;
-            //clear();
-            //std::cout<<bajtyZ*100/dozapis<<"% zapisano "<<bajtyZ<<" bajtow"<<std::endl;
-            //clear();
-        }
-        //std::cout<<std::endl;
-        plik<<std::endl;
-
-    }
-
-    /*
-    std::cout<<std::endl;
-    for (int iy=0;iy<=ySize+1;iy++)
-    {
-        for (int ix=0;ix<=xSize+1;ix++)
-        {
-            if (kierunki[ix-1][iy-1]==-1) std::cout<<"#";
-            else if (kierunki[ix-1][iy-1]==-2) std::cout<<"#";
-            else if (kierunki[ix-1][iy-1]==0) std::cout<<" ";
-            else if (kierunki[ix-1][iy-1]==1) std::cout<<">";
-            else if (kierunki[ix-1][iy-1]==2) std::cout<<"v";
-            else if (kierunki[ix-1][iy-1]==3) std::cout<<"<";
-            else if (kierunki[ix-1][iy-1]==4) std::cout<<"^";
+            (short)maze[x][y] ? std::cout<<char(219) : std::cout<<' ';
         }
         std::cout<<std::endl;
     }
-    */
-    plik.close();
+
+    return 0;
 }
